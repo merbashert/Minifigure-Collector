c9e8501584cb419a3e77a91533e344c43236a4d1
Meredith Bashert <merbashert@gmail.com>
2019-11-06 15:11:31 -0500

    pushing for Heroku testing

--- controllers/legos.js
+++ controllers/legos.js
@@ -16,17 +16,35 @@ router.get('/', (req, res) => {
 
 router.get('/new', (req, res) => {
     res.render('new.ejs');
-})
+});
+
+
+//Search graveyard
+// router.get('/results/:name', (req, res) => {
+//     Lego.find({}, (err, foundLego) => {
+//         res.render(
+//             'results.ejs',
+//             {
+//                 legos: foundLego
+//             }
+//         );
+//     });
+// });
+
+// router.get('/results/:name', (req, res) => {
+//     console.log(req.params);
+// });
 
 router.get('/:id', (req, res) => {
     Lego.findById(req.params.id, (err, foundLego) => {
         res.render(
             'show.ejs', {
                 lego: foundLego
-            });
-        }
-    );
+            }
+        );
+    });
 });
+
 
 router.put('/:id', (req, res) => {
     Lego.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedLego) => {
@@ -38,13 +56,29 @@ router.get('/:id/edit', (req, res) => {
     Lego.findById(req.params.id, (err, foundLego) => {
         res.render(
             'edit.ejs',
-        {
-            lego:foundLego
-        })
+            {
+                lego:foundLego
+            }
+        );
     })
 })
 
 router.post('/', (req, res) => {
+    if(req.body.happyguy === "on") {
+        req.body.img = "/images/new/happy_guy.jpg"
+    } if(req.body.happygirl === "on") {
+        req.body.img = "/images/new/happy_girl.jpg"
+    }if(req.body.angryguy === "on") {
+        req.body.img = "/images/new/angry_guy.jpg"
+    }if(req.body.angrygirl === "on") {
+        req.body.img = "/images/new/angry_girl.jpg"
+    }if(req.body.scaredguy === "on") {
+        req.body.img = "/images/new/scared_guy.jpg"
+    }if(req.body.scaredgirl === "on") {
+        req.body.img = "/images/new/scared_girl.jpg"
+    }
+    req.body.series = "Future Lego Series"
+    req.body.year = 2019
     Lego.create(req.body, (error, createdLego) => {
         res.redirect('/legos')
     })
--- models/legos.js
+++ models/legos.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const legoSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    series: {type: Number},
+    series: {type: String},
     year: {type: Number},
     img: {type: String}
 })
