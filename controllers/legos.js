ca39a5a02b364b6967c0be12681b9bcd82ad2524
Meredith Bashert <merbashert@gmail.com>
2019-11-06 18:41:26 -0500

    created series page

--- controllers/legos.js
+++ controllers/legos.js
@@ -6,39 +6,55 @@ const router = express.Router();
 router.get('/seed', (req, res)=>{
     Lego.create([
 
-        ], (err, data)=>{
-            res.redirect('/legos');
-        })
-    });
+    ], (err, data)=>{
+        res.redirect('/legos');
+    })
+});
 
 router.get('/', (req, res) => {
-    Lego.find({}, (error, legos) => {
-        res.render(
-            'legos/index.ejs',
-            {
-                legos:legos
-            }
-        );
-    });
+    if(req.session.username) {
+        Lego.find({}, (error, legos) => {
+            res.render(
+                'legos/index.ejs',
+                {
+                    legos:legos
+                }
+            );
+        });
+    } else {
+        res.redirect('/')
+    }
+
 });
 
 router.get('/new', (req, res) => {
     res.render('legos/new.ejs');
 });
 
+router.get('/:id/series', (req, res) => {
+    Lego.find({series: req.params.id}, (err, foundLego) => {
+        res.render(
+            'legos/results.ejs',
+            {
+                legos: foundLego,
+                series: req.params.id
+            }
+        );
+    });
+});
 
-// Search graveyard
-// router.get('/results/', (req, res) => {
-//     Lego.find({}, (err, foundLego) => {
+//Search Graveyard
+// router.get('/:id/search', (req, res) => {
+//     Lego.find({name: "Cowboy"}, (err, foundLego) => {
 //         res.render(
-//             'results.ejs',
+//             'legos/search.ejs',
 //             {
-//                 legos: foundLego
+//                 legos:foundLego
 //             }
-//         );
-//     });
-// });
-
+//
+//         )
+//     })
+// })
 // router.get('/results/:name', (req, res) => {
 //     console.log(req.params);
 // });
@@ -73,31 +89,31 @@ router.get('/:id/edit', (req, res) => {
 
 
 
-    router.post('/', (req, res) => {
-        if(req.body.happyguy === "on") {
-            req.body.img = "/images/new/happy_guy.jpg"
-        } if(req.body.happygirl === "on") {
-            req.body.img = "/images/new/happy_girl.jpg"
-        }if(req.body.angryguy === "on") {
-            req.body.img = "/images/new/angry_guy.jpg"
-        }if(req.body.angrygirl === "on") {
-            req.body.img = "/images/new/angry_girl.jpg"
-        }if(req.body.scaredguy === "on") {
-            req.body.img = "/images/new/scared_guy.jpg"
-        }if(req.body.scaredgirl === "on") {
-            req.body.img = "/images/new/scared_girl.jpg"
-        }
-        req.body.series = "Future Lego Series"
-        req.body.year = 2019
-        Lego.create(req.body, (error, createdLego) => {
-            res.redirect('/legos')
-        })
+router.post('/', (req, res) => {
+    if(req.body.happyguy === "on") {
+        req.body.img = "/images/new/happy_guy.jpg"
+    } if(req.body.happygirl === "on") {
+        req.body.img = "/images/new/happy_girl.jpg"
+    }if(req.body.angryguy === "on") {
+        req.body.img = "/images/new/angry_guy.jpg"
+    }if(req.body.angrygirl === "on") {
+        req.body.img = "/images/new/angry_girl.jpg"
+    }if(req.body.scaredguy === "on") {
+        req.body.img = "/images/new/scared_guy.jpg"
+    }if(req.body.scaredgirl === "on") {
+        req.body.img = "/images/new/scared_girl.jpg"
+    }
+    req.body.series = "Future Lego Series"
+    req.body.year = 2019
+    Lego.create(req.body, (error, createdLego) => {
+        res.redirect('/legos')
     })
+})
 
-    router.delete('/:id', (req, res) => {
-        Lego.findByIdAndRemove(req.params.id, (err, data) => {
-            res.redirect('/legos')
-        })
+router.delete('/:id', (req, res) => {
+    Lego.findByIdAndRemove(req.params.id, (err, data) => {
+        res.redirect('/legos')
     })
+})
 
-    module.exports = router;
+module.exports = router;
